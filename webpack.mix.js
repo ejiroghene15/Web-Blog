91b7b6ea4f72b66bc5ee0794b1b07df52ef63b28
Ejiroghene <ejiroghene15@gmail.com>
2020-03-15 14:37:22 +0100

    config and json files

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,5 @@ const mix = require("laravel-mix");
  */
 
 mix.js("resources/js/main.js", "public/js")
-    .js("resources/js/dashboard.js", "public/js")
-    .sass("resources/sass/dashboard.scss", "public/css")
     .sass("resources/sass/main.scss", "public/css");
+ 

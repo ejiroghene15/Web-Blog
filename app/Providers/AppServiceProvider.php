23c612368060d3872d898f33a286720bf6e14498
Ejiroghene <ejiroghene15@gmail.com>
2020-03-26 21:21:03 +0100

    :recycle: little changes on providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,55 +8,53 @@ use App\Posts;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
+	/**
+	 * Bootstrap any application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		view()->share('categories', Categories::get()); # pass the categories to any view on which it will be rendered
+		view()->composer(['index'], function ($view) {
+			// * function for displaying a portion of the the post.. this is called an excerpt
+			$featured = function ($text, $numb) {
+				if (strlen($text) > $numb) {
+					$text = substr($text, 0, $numb);
+					$text = substr($text, 0, strrpos($text, ", ")) . "<strong>....</strong>";
+				}
+				return $text;
+			};
 
-        view()->composer(['layouts.nav', 'newpost',  'editpost', 'index'], function ($view) {
-
-            $featured = function ($text, $numb) {
-                if (strlen($text) > $numb) {
-                    $text = substr($text, 0, $numb);
-                    $text = substr($text, 0, strrpos($text, ", ")) . "<strong>....</strong>";
-                }
-                return $text;
-            };
-
-            $excerpts = [];
-            if (Posts::exists()) {
-                // * get the excerpts  and paginate them
-                $perpage = 1;
-                $page = request('page');
-                $offset = $perpage * (int) $page;
-                $excerpts = Posts::find(1)->orderBy('created_at', 'desc')->paginate($perpage);
-                $posts_id = $excerpts->map(function ($item, $key) {
-                    //echo $item['id'];
-                    return $item['id'];
-                });
-                // * get the related post not among the paginated excerpts
-                $relatedposts = Posts::whereNotIn('id', $posts_id)
-                    ->orderBy('created_at', 'desc')
-                    ->limit(1)
-                    ->inRandomOrder()
-                    ->get();
-            }
-
-            $view->with('categories', Categories::get()); # pass the categories to any view on which it will be rendered
-            $view->with(compact('featured', 'excerpts', 'relatedposts'));
-        });
-    }
+			$excerpts = $relatedposts = [];
+			if (Posts::exists()) {
+				// * get the excerpts  and paginate them
+				$perpage = 1;
+				$page = request('page');
+				$offset = $perpage * (int) $page;
+				$excerpts = Posts::find(1)->orderBy('created_at', 'desc')->paginate($perpage);
+				$posts_id = $excerpts->map(function ($item, $key) {
+					//echo $item['id'];
+					return $item['id'];
+				});
+				// * get the related post not among the paginated excerpts
+				$relatedposts = Posts::whereNotIn('id', $posts_id)
+					->orderBy('created_at', 'desc')
+					->limit(1)
+					->inRandomOrder()
+					->get();
+			}
+			$view->with(compact('featured', 'excerpts', 'relatedposts'));
+		});
+	}
 }

318cdc07b3569c40836fe896221384c134e1ff1a
Ejiroghene <ejiroghene15@gmail.com>
2020-03-26 21:07:18 +0100

    :construction:  in progress

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,20 +2,19 @@
 
 namespace App\Http\Controllers;
 
-#use App\Posts;
 use App\Categories;
 
 class PagesController extends Controller
 {
-    function index()
-    {
-        return view('index');
-    }
+	function index()
+	{
+		return view('index');
+	}
 
-    function category($category)
-    {
-        $cat_id = Categories::where('category', $category)->value('id');
-        $posts = Categories::find($cat_id)->posts()->get();
-        return view('category', compact(['category', 'posts']));
-    }
+	function category($category)
+	{
+		$cat_id = Categories::where('category', $category)->value('id');
+		$posts = Categories::find($cat_id)->posts()->get();
+		return view('category', compact(['category', 'posts']));
+	}
 }

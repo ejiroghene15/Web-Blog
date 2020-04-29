c349a56a386022fcab59288854af7ad38e5fafb1
Ejiroghene <ejiroghene15@gmail.com>
2020-04-29 10:07:08 +0100

    admin functionalities

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Categories;
+use App\Posts;
+use App\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+
+    function index()
+    {
+        return view("admin")
+            ->withUsers(User::all())
+            ->withPosts(Posts::all());
+    }
+
     function addCategory(Request $request)
     {
         $category = $request->validate([
@@ -24,4 +34,46 @@ class AdminController extends Controller
         Categories::destroy($request->cat_id);
         return back()->with('message', 'Category deleted');
     }
+
+    function userProfile(User $user)
+    {
+        return view('admin.user.profile')->withUser($user);
+    }
+
+    function userAccount(User $user)
+    {
+        switch (request('action')) {
+            case "lock":
+                return $this->lockAccount($user);
+                break;
+            case "unlock":
+                return $this->unlockAccount($user);
+                break;
+            case "delete":
+                return $this->deleteAccount($user);
+                break;
+            default:
+        }
+    }
+
+    function lockAccount($user)
+    {
+        $user->account_status = "blocked";
+        $user->save();
+        return back()->withMessage("Account suspended");
+    }
+
+    function unlockAccount($user)
+    {
+        $user->account_status = "active";
+        $user->save();
+        return back()->withMessage("Account unsuspended");
+    }
+
+    function deleteAccount(User $user)
+    {
+        $user->delete();
+        return redirect('/admin')->withMessage("User account has been deleted successfully");
+    }
+
 }

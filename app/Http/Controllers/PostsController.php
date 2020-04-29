4a5f847c41e90e1d493f2ffd86f28d4cee3d6bf6
Ejiroghene <ejiroghene15@gmail.com>
2020-04-29 10:08:32 +0100

    added slug for posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,162 +2,125 @@
 
 namespace App\Http\Controllers;
 
-date_default_timezone_set('Africa/Lagos');
-
 use App\PostComments;
 use App\PostLikes;
 use App\Posts;
 use App\PostViews;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\Validator;
 
 class PostsController extends Controller
 {
-	
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return \Illuminate\Http\Response
-	 */
-	public function create()
-	{
-		return view('newpost');
-	}
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return \Illuminate\Http\Response
-	 */
-	public function store(Request $request)
-	{
-		$user = Auth::user();
-		$post = $request->all();
-		Validator::make($post, [
-			'cat_id' => 'required',
-			'title' => 'required|unique:posts',
-			'body' => 'required'
-		], [
-			'cat_id.required' => "You have to select a category for your post",
-			'title.unique' => "There's already a post with this title, please enter a different title for your post"
-		])->validate();
-		$post['author'] = $user->name;
-		$post['author_id'] = $user->id;
-		Posts::create($post);
-		return redirect()->action("PostsController@create")->with("created", "Your post has been created");
-	}
+    public function create()
+    {
+        return view('post.create');
+    }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int	$id
-	 * @return \Illuminate\Http\Response
-	 */
-	// * get the full post of an article along with its comments and related articles
-	public function show($title)
-	{
-		// ? remove the dashes added to the title of the post
-		$title = str_replace('-', ' ', $title);
-		$article = Posts::where('title', $title)->first();
+    public function store()
+    {
+        $post = request()->validate([
+            'cat_id' => 'required',
+            'title' => 'required|unique:posts',
+            'body' => 'required'
+        ], [
+            'cat_id.required' => "You have to select a category for your post",
+            'title.unique' => "There's already a post with this title, please enter a different title for your post"
+        ]);
+        $post['author_id'] = Auth::id();
+        $post['title_slug'] = str_slug($post['title'], '-');
+        Posts::create($post);
+        return redirect()->route('preview', $post['title_slug'])->withMessage("Your post has been created");
+    }
 
-		// no of views on a post, only users who are logged in
-		if (Auth::check()) {
-			$viewed_post = Posts::find($article->id)->view()->where('user_id', auth()->id())->count();
-			if (!$viewed_post) {
-				PostViews::firstOrCreate(['post_id' =>	$article->id, 'user_id' => auth()->id()]);
-				Posts::where('id', $article->id)->increment('views');
-			}
-		}
+    // * get the full post of an article along with its comments and related articles
+    public function show($title)
+    {
+        $article = Posts::where([
+            ['is_approved', '=', 1],
+            ['title_slug', '=', $title]
+        ])->firstOrFail();
+        // no of views on a post, only users who are logged in
+        if (Auth::check()) {
+            $viewed_post = Posts::find($article->id)->view()->where('user_id', Auth::id())->count();
+            if (!$viewed_post) {
+                PostViews::firstOrCreate(['post_id' => $article->id, 'user_id' => Auth::id()]);
+                Posts::where('id', $article->id)->increment('views');
+            }
+        }
 
-		$related_articles = Posts::where('title', '<>', $title)->get();
-		return view('article')->with(compact('article', 'related_articles'));
-	}
+        $related_articles = Posts::where('id', '<>', $article->id)
+            ->inRandomOrder()
+            ->take(2)
+            ->get();
+        return view('post.show')->with(compact('article', 'related_articles'));
+    }
 
-	public function comment(Request $request)
-	{
-		$user = Auth::user();
-		$comment = $request->all();
-		Validator::make($comment, [
-			'comment' => 'required'
-		], [
-			'required' => "You can't post an empty comment"
-		])->validate();
-		$comment['user_id'] = $user->id;
-		PostComments::create($comment);
-		return back();
-	}
+    public function comment(Posts $post)
+    {
+        $comment = request()->validate([
+            'comment' => 'required'
+        ], [
+            'required' => "You can't post an empty comment"
+        ]);
 
-	public function react(Request $request)
-	{
-		$hearts = $request->hearts;
-		$post_id = $request->postid;
-		$user_id = $request->userid;
+        $post->comments()->create(
+            [
+                "comment" => $comment['comment'],
+                "user_id" => Auth::id()
+            ]
+        );
+        return back();
+    }
 
-		$_has_reacted = Posts::find($post_id)->reaction()->where('user_id', $user_id)->count();
-		if ($_has_reacted == 0) {
-			PostLikes::firstOrCreate(['post_id' =>	$post_id, 'user_id' => $user_id]);
-			Posts::where('id', $post_id)->increment('love');
-		} else {
-			if ($hearts == 1) {
-				Posts::where('id', $post_id)->increment('love');
-			} else {
-				Posts::where('id', $post_id)->decrement('love');
-				Posts::find($post_id)->reaction()->where('user_id', $user_id)->delete();
-			}
-		}
-	}
+    public function react()
+    {
+        $hearts = request('hearts');
+        $post_id = request('postid');
+        $user_id = request('userid');
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int	$id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function edit($title)
-	{
-		// ? remove the dashes added to the title of the post
-		$title = str_replace('-', ' ', $title);
-		$article = Posts::where('title', $title)->first();
-		if (Gate::allows('update-post', $article)) {
-			return view('editpost')->with(compact('article'));
-		} else {
-			echo "You can't edit this post";
-		}
-	}
+        $_has_reacted = Posts::find($post_id)->reaction()->where('user_id', $user_id)->count();
+        if ($_has_reacted == 0) {
+            PostLikes::firstOrCreate(['post_id' => $post_id, 'user_id' => $user_id]);
+            Posts::where('id', $post_id)->increment('love');
+        } else {
+            if ($hearts == 1) {
+                Posts::where('id', $post_id)->increment('love');
+            } else {
+                Posts::where('id', $post_id)->decrement('love');
+                Posts::find($post_id)->reaction()->where('user_id', $user_id)->delete();
+            }
+        }
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  int	$id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function update(Request $request, $id)
-	{
-		Validator::make($request->all(), [
-			'cat_id' => 'required',
-			'title' => 'required|unique:posts',
-			'body' => 'required'
-		], [
-			'cat_id.required' => "You have to select a category for your post",
-			'title.unique' => "There's already a post with this title, please enter a different title for your post"
-		])->validate();
-		Posts::where('id', $id)->update(['title' => $request->title, 'cat_id' => $request->cat_id, 'body' => $request->body]);
-		return redirect()->action("PostsController@edit", ['title' => str_replace(' ', '-', $request->title)])->with("updated", "Your post has been updated");
-	}
+    public function edit($title)
+    {
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int	$id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function destroy($id)
-	{
-		Posts::destroy($id);
-		return back();
-	}
+        $article = Posts::where('title_slug', $title)->first();
+        if (Gate::allows('update-post', $article)) {
+            return view('post.edit')->with(compact('article'));
+        } else {
+            return back();
+        }
+    }
+
+    public function update(Posts $post)
+    {
+        $slug = str_slug(request('title'), '-');
+        $post['title_slug'] = $slug;
+        $post->update(request()->validate([
+            'cat_id' => 'required',
+            'title' => 'required',
+            'body' => 'required'
+        ], [
+            'cat_id.required' => "You have to select a category for your post",
+        ]));
+        return redirect()->route("preview", $slug)->withMessage("Your post has been updated");
+    }
+
+    public function destroy(Posts $post)
+    {
+        Posts::destroy($post);
+        return back();
+    }
 }

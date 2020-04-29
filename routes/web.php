9c4b1886ef4f6da319cea8bfdee298c6923f006f
Ejiroghene <ejiroghene15@gmail.com>
2020-04-29 09:31:07 +0100

    modified routes to reflect changes

--- routes/web.php
+++ routes/web.php
@@ -11,44 +11,41 @@
   |
  */
 
-use Illuminate\Support\Facades\Auth;
 
 Auth::routes(['verify' => true]);
-Route::get('/', 'PagesController@index')->middleware('guest');
+Route::get('/', 'PagesController@index');
 Route::get('category/{id}', 'PagesController@category');
 Route::get('article/{title}', 'PostsController@show')->name('article');
 // Route::get('send-mail', function () {
-//     return (new MailtrapExample(['name'=> "test" ]))->render();
+//     return (new \App\Mail\MailtrapExample(['name' => "test"]))->render();
 // });
-Route::get('/verifyaccount', function () {
-	return view('verifyaccount');
-});
+
+Route::get('verifyaccount', 'UserController@verifyAccount');
+
 Route::group(['middleware' => ['auth']], function () {
-	Route::get('home', 'PagesController@index')->name('home');
-	Route::get('new_category', function () {
-		return view('create_category');
-	});
+    Route::get('newpost',  'PostsController@create');
+    Route::post('newpost', 'PostsController@store');
+    Route::get('editpost/{title}', 'PostsController@edit')->name('edit_post');
+    Route::put('updatepost/{post}', 'PostsController@update')->name('updatepost');
+    Route::delete('deletepost/{post}', 'PostsController@destroy')->name('deletepost');
+    Route::post('article/{post}', 'PostsController@comment');
+    Route::get('profile', 'UserController@show');
+    Route::put('profile', 'UserController@update');
+    Route::get('archive', 'UserController@archive');
 
-	Route::post('article/{title}', 'PostsController@comment');
-	Route::get('newpost',  'PostsController@create')->name('create_post');
-	Route::post('newpost', 'PostsController@store');
-	Route::get('editpost/{title}', 'PostsController@edit')->name('edit_post');
-	Route::put('editpost/{id}', 'PostsController@update')->name('editpost');
-	Route::delete('deletepost/{id}', 'PostsController@destroy')->name('deletepost');
-	Route::get('profile', 'UserController@show')->name('profile');
-	Route::put('profile', 'UserController@update');
-	Route::get('archive', 'UserController@archive')->name('archive');
+    ################## Admin  ######################
 
-	################## Admin Functions ######################
-	
-	Route::get('admin/category', function () {
-		return view('admin.category');
-	});
-	Route::post('admin/category', 'AdminController@addCategory')->name('add_category');
-	Route::delete('admin/category', 'AdminController@deleteCategory')->name('delete_category');
+    Route::group(['middleware' => ['is_admin']], function () {
+        Route::get('admin', 'AdminController@index');
+        Route::post('admin/category', 'AdminController@addCategory')->name('add_category');
+        Route::delete('admin/category', 'AdminController@deleteCategory')->name('delete_category');
 
-	Route::get('logout', function () {
-		Auth::logout();
-		return redirect('/');
-	});
+        Route::get('admin/userprofile/{user}', 'AdminController@userProfile')->name('userprofile');
+        Route::post('admin/userprofile/{user}', 'AdminController@userAccount')->name('action');
+    });
+
+    Route::get('preview/article/{post}', 'PreviewPost@show')->name('preview');
+    Route::post('preview/article/{post}', 'PreviewPost@action');
+
+    Route::get('logout', 'UserController@logout');
 });

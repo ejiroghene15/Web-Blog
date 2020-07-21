8e5514f69f5506f4f937e136977e5c211e40f2fe
ejiroghene15 <ejiroghene15@gmail.com>
2020-07-21 21:10:48 +0100

    Deploying cms to heroku

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -6,32 +6,30 @@ use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		User::create(
-			[
-				'name' => 'Ejiroghene Obiuwevbi',
-				'email' => 'ejiroghene15@gmail.com',
-				'email_verified_at' => now(),
-				'account_status' => 'active',
-				'username' => 'Ejiroghene',
-				'password' => Hash::make('password'),
-				'is_admin' => 1,
-			],
-			[
-				'name' => 'Kohwo Akpos',
-				'email' => 'kohwoakpos@gmail.com',
-				'email_verified_at' => now(),
-				'account_status' => 'active',
-				'username' => 'Akpos',
-				'password' => Hash::make('password'),
-				'is_admin' => 1,
-			]
-		);
-	}
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Ejiroghene Obiuwevbi',
+            'email' => 'ejiroghene15@gmail.com',
+            'email_verified_at' => now(),
+            'account_status' => 'active',
+            'username' => 'Ejiroghene',
+            'password' => Hash::make('password'),
+            'is_admin' => 1
+        ]);
+        User::create([
+            'name' => 'Kohwo Akpos',
+            'email' => 'kohwoakpos@gmail.com',
+            'email_verified_at' => now(),
+            'account_status' => 'active',
+            'username' => 'Akpos',
+            'password' => Hash::make('password'),
+            'is_admin' => 1
+        ]);
+    }
 }

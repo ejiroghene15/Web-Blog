6f7fe9115e224f6f0065706281d141004cd1c7be
ejiroghene15 <ejiroghene15@gmail.com>
2020-07-21 18:44:13 +0100

    Deploying cms to heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // * pass the categories to any view on which it will be rendered
-        view()->share('categories', Categories::get());
+        // view()->share('categories', Categories::get());
         view()->composer(['index'], function ($view) {
             // * function for displaying a portion of the the post.. this is called an excerpt
             $featured = function ($text, $numb) {

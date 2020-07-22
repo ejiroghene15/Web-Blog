ba497de70fc8744e9b7814c4fcb2acbd36d4c647
ejiroghene15 <ejiroghene15@gmail.com>
2020-07-22 02:32:01 +0100

    minor edits, deploying to heroku

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -53,7 +53,7 @@
 								<article class="card border-0">
 
 									<section class=" card-header">
-										<div class="d-flex justify-content-between align-items-start mb-1 p-2">
+										<div class="d-flex justify-content-between align-items-start mb-1 py-2">
 											<p class="h5 heading m-0">{{$article->title}}</p>
 											<p class="category badge badge-info p-2 m-0 gc-p">
 												{{$article->category->category}}

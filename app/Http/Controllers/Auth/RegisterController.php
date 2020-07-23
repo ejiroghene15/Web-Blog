02fe18ec1bcd8da124e7768515b48d4c15e64b55
ejiroghene15 <ejiroghene15@gmail.com>
2020-07-23 02:17:31 +0100

    fixes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,15 +68,15 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         $data['verification_token'] = str_random();
-        // User::create([
-        //     'name' => $data['name'],
-        //     'username' => $data['username'],
-        //     'email' => $data['email'],
-        //     'verification_token' => $data['verification_token'],
-        //     'password' => Hash::make($data['password']),
-        // ]);
+        return User::create([
+            'name' => $data['name'],
+            'username' => $data['username'],
+            'email' => $data['email'],
+            'verification_token' => $data['verification_token'],
+            'password' => Hash::make($data['password']),
+        ]);
 
         // Mail::to($data['email'])->send(new EmailVerification($data));
-         session()->flash("message", "Your registration was successful, A verification link has been sent to your email address, Please verify your account before logging in");
+        session()->flash("message", "Your registration was successful, A verification link has been sent to your email address, Please verify your account before logging in");
     }
 }
